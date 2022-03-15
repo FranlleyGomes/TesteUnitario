975c7af50b293db5ee27f547b27dbc076b0d9722
FranlleyGomes <58038727+FranlleyGomes@users.noreply.github.com>
2022-03-15 14:41:17 -0300

    Update Pessoa.pas
    AreEquals e IsNotEmpty

--- Pessoa.pas
+++ Pessoa.pas
@@ -1,16 +1,28 @@
 unit Pessoa;
 
+
+
 interface
 
 type
 
   TPessoa = class
+  private
+    FNome: String;
+    procedure SetNome(const Value: String);
+  published
     function TratarCPFCNJP(aValue: String): String;
+    property Nome : String read FNome write SetNome;
   end;
 
 implementation
 
 { TPessoa }
+
+procedure TPessoa.SetNome(const Value: String);
+begin
+  FNome := Value;
+end;
 
 function TPessoa.TratarCPFCNJP(aValue: String): String;
 var
@@ -22,6 +34,8 @@ begin
            Result := Result + aValue[I];
 
     end;
+
+    Result := Result + 'X';
 
 end;
 
